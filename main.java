355a33dde53110d8a52f9a47b405446743877938
moroshan1 <noam.moroshan@gmail.com>
2025-06-26 20:01:43 +0300

    functions

---
+++ main.java
@@ -0,0 +1,17 @@
+public class main {
+public boolean isAtTop(){
+    return false;
+}
+public boolean isAtButton(){
+    return false;
+}
+public void goToTop(){
+
+}
+public void goToButton(){
+
+}
+public boolean isAtTarget(){
+    return true;
+}
+}
